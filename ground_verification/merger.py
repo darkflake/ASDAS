f25e9ee92181f5d21ba9d51a7ff48dcbcdca0647
sudhanshu <sudhanshu.1.k@gmail.com>
2020-03-13 23:37:41 +0530

    Support for Interpolation + Savitsy-Golay Filter

--- ground_verification/merger.py
+++ ground_verification/merger.py
@@ -13,11 +13,79 @@ def merge(label):
     verification_df.sort_values(by=['lat', 'long'], inplace=True)
     verification_df.reset_index(inplace=True)
 
-    actual_pointer = 0
+    # max_long = actual_df['lat'].max()
+    # min_long = actual_df['lat'].min()
+    # print(max_long)
+    # print(min_long)
+    # print((float(max_long) - float(min_long)) * 1000000)
+    # exit()
+
     actual_list = []
+    actual_pointer = 0
+    index = 0
+    # for index in verification_df.index:
+    #     actual_lat, actual_long = get_actual(actual_pointer, actual_df)
+    #
+    #     while float(verification_df['lat'][index]) == actual_lat and float(verification_df['long'][index]) == actual_long:
+    #         print("in while")
+    #         actual_list.append(actual_pointer)
+    #         actual_pointer += 1
+    #         actual_lat, actual_long = get_actual(actual_pointer, actual_df)
+    #
+    # for index in verification_df.index:
+    #     actual_lat, actual_long = get_actual(actual_pointer, actual_df)
 
-    for index in verification_df.index:
-        actual_pointer = actual_list[-1]
+    while True:
+        ver_lat = round(float(verification_df['lat'][index]), 4)
+        ver_long = round(float(verification_df['long'][index]), 4)
+        actual_lat, actual_long = get_actual(actual_pointer, actual_df)
+
+        print(index)
+        print("ap : " + str(actual_pointer))
+        print("a : " + str(actual_lat) + ", " + str(actual_long))
+        print("v : " + str(ver_lat) + ", " + str(ver_long))
+
+        if actual_lat < ver_lat:
+            actual_pointer += 1
+            continue
+        elif ver_lat == actual_lat and actual_long < ver_long:
+            actual_pointer += 1
+            continue
+        elif ver_lat == actual_lat and ver_long == actual_long:
+            print("a" + str(actual_pointer))
+            actual_pointer += 1
+            actual_list.append(actual_pointer)
+        elif ver_lat == actual_lat and actual_long > ver_long:
+            actual_pointer += 1
+            continue
+        elif actual_lat > ver_lat:
+            index += 1
+            if index == 20:
+                break
+            continue
 
 
-merge('vegetation')
+
+        # while float(verification_df['lat'][index]) < actual_lat:
+        #     actual_pointer += 1
+        #     continue
+        # while float(verification_df['long'][index]) < actual_long:
+        #     actual_pointer += 1
+        #     continue
+        # while float(verification_df['lat'][index]) == actual_lat and float(verification_df['long'][index]) == actual_long:
+        #     print("in while")
+        #     actual_list.append(actual_pointer)
+        #     actual_pointer += 1
+        #     actual_lat, actual_long = get_actual(actual_pointer, actual_df)
+
+    return actual_list
+
+
+def get_actual(pointer, data_frame):
+    actual_lat = round(float(data_frame['lat'][pointer]), 4)
+    actual_long = round(float(data_frame['long'][pointer]), 4)
+    # print(actual_lat, actual_long)
+    return actual_lat, actual_long
+
+
+print(merge('vegetation'))
---
+++ interpolation.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+from savitsky_golay import savitzky_golay
+import numpy as np
+
+
+def interpolate(raw_data: list, x_values: str, interpolation_points: list):
+    interpolated_data = raw_data.copy()                                              # Retain original data:
+
+    for x, y in list(zip(interpolation_points, interpolation_points[1:]))[::2]:      # Read interpolation points from list
+        x, y = x_values.index(x), x_values.index(y)                                  # Get the number of entry on X-Axis
+        slope = (raw_data[y] - raw_data[x])/(y-x)                                    # Slope of line in the two points
+
+        for i in range(x+1, y):                                                      # Calculate Y value for every point
+            interpolated_data[i] = interpolated_data[x] + (i-x)*slope                # y = mx + c
+
+    graph(raw_data, x_values, interpolated_data)                                     # Render original + interpolated data
+
+
+# Render graphs:
+def graph(data_y, data_x, interpolated_data=None):
+    plt.plot(data_y, '-bo', label='Actual Data')
+
+    sav = savitzky_golay(y=np.asarray(data_y), window_size=5, order=3)
+    plt.plot(sav, '--g', label='SavGol Filter')
+
+    if interpolated_data:
+        plt.plot(interpolated_data, ':ro', label='Interpolated Data')
+        sav_interpolated = savitzky_golay(y=np.asarray(interpolated_data), window_size=5, order=3)
+        plt.plot(sav_interpolated, '--y', label='SavGol Filter (interpolated)')
+
+    plt.xlabel('x label')
+    plt.ylabel('y label')
+    plt.title('Interpolation')
+
+    plt.xticks(range(0, len(data_y)), data_x)
+    plt.grid(color='grey', linestyle='-', linewidth=0.25, alpha=0.5)
+    plt.legend()
+
+    plt.show()
+
+
+# Play :
+basic_y = [10, 20, 40, 60, 70, 30, 20, 40, 80, 110, 160, 120, 100, 180, 200]
+basic_x = [str(d)+"/1" for d in range(1, 16)]
+
+interpolate(basic_y, basic_x, ['5/1', '11/1', '11/1', '14/1'])
