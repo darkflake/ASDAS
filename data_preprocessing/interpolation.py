d2df003e562972cd343d50699d07c89bb3f13576
sudhanshu <sudhanshu.1.k@gmail.com>
2020-03-16 17:48:15 +0530

    minor updates

--- data_preprocessing/interpolation.py
+++ data_preprocessing/interpolation.py
@@ -2,41 +2,47 @@ import matplotlib.pyplot as plt
 from data_preprocessing.savitsky_golay import savitzky_golay
 import numpy as np
 
+labels = ['05 Jan', '04 Feb', '01 Mar', '05 Apr', '05 May', '04 Jun', '04 Jul', '03 Aug', '02 Sep', '02 Oct', '01 Nov', '01 Dec']
+indexes = [0, 6, 11, 18, 24, 30, 36, 42, 48, 54, 60, 66]
 
-def interpolate(raw_data: list, x_values: str, interpolation_points: list):
+
+def interpolate(raw_data: list, x_values: str, interpolation_points: list, title: str):
     interpolated_data = raw_data.copy()                                              # Retain original data:
 
     for x, y in list(zip(interpolation_points, interpolation_points[1:]))[::2]:      # Read interpolation points from list
-        x, y = x_values.index(x), x_values.index(y)                                  # Get the number of entry on X-Axis
+        x, y = x_values.index(x)-2, x_values.index(y)-2
         slope = (raw_data[y] - raw_data[x])/(y-x)                                    # Slope of line in the two points
 
         for i in range(x+1, y):                                                      # Calculate Y value for every point
             interpolated_data[i] = interpolated_data[x] + (i-x)*slope                # y = mx + c
 
-    graph(raw_data, x_values, interpolated_data)                                     # Render original + interpolated data
+    graph(raw_data, title=title, interpolated_data=interpolated_data)                # Render original + interpolated data
 
 
 # Render graphs:
-def graph(data_y, data_x, interpolated_data=None):
-    plt.plot(data_y, '-bo', label='Actual Data')
-
-    sav = savitzky_golay(y=np.asarray(data_y), window_size=5, order=3)
-    plt.plot(sav, '--g', label='SavGol Filter')
+def graph(data_y, title='Data', interpolated_data=None):
+    plt.plot(data_y, '-go', label='Actual Data(NDVI)', alpha=0.3)
 
     if interpolated_data:
-        plt.plot(interpolated_data, ':ro', label='Interpolated Data')
-        sav_interpolated = savitzky_golay(y=np.asarray(interpolated_data), window_size=5, order=3)
-        plt.plot(sav_interpolated, '--y', label='SavGol Filter (interpolated)')
+        plt.plot(interpolated_data, ':r', label='Interpolated Data', alpha=0.7)
+        sav = savitzky_golay(y=np.asarray(interpolated_data), window_size=9, order=4)
 
-    plt.xlabel('x label')
-    plt.ylabel('y label')
-    plt.title('Interpolation')
+    else:
+        sav = savitzky_golay(y=np.asarray(data_y), window_size=7, order=3)
 
-    plt.xticks(range(0, len(data_y)), data_x)
+    plt.plot(sav, '--b', label='SavGol Filter')
+    plt.xlabel('2019')
+    plt.ylabel('Band Value')
+    plt.title(f"Pixel: {title}")
+    plt.ylim([-0.3, 1])
+
+    plt.xticks(indexes, labels, rotation=20)
     plt.grid(color='grey', linestyle='-', linewidth=0.25, alpha=0.5)
     plt.legend()
 
     plt.show()
+    plt.savefig(f'{title}.png')
+    # plt.close()
 
 
 # Play :
--- data_preprocessing/main.py
+++ data_preprocessing/main.py
@@ -1,11 +1,35 @@
 import os
 import pandas as pd
 
-from data_preprocessing.interpolation import graph
+from data_preprocessing.interpolation import interpolate, graph
 
-Green_csv = pd.read_csv(os.path.abspath(__file__ + "/../../")+"/data_2019/NIR.csv")
+# title = input("Band to plot : ")
+# pixel_index = int(input("Pixel Index : "))
+title = "NDVI"
+pixel_index = 0
+input_csv = pd.read_csv(os.path.abspath(__file__ + "/../../")+f"/data_2019/{title}.csv")
 
-# print(Blue_csv.values.tolist()[0])
-# print(list(Blue_csv))
+# print(input_csv.columns.tolist())
+# for pixel_index in range(0, 27):
 
-graph(Green_csv.values.tolist()[1][2:], list(Green_csv))
+# columnns = input_csv.columns.tolist()[2:]
+# for entry in columnns:
+#     month = entry[5:7]
+#     print(month)
+#     if month == init_month:
+#         continue
+#     else:
+#         labels.append(entry)
+#         indexes.append(columnns.index(entry))
+#         init_month = month
+#
+# print(labels)
+# print(indexes)
+
+print(f"Min value : {min(input_csv.values.tolist()[pixel_index][2:])} , Max value : {max(input_csv.values.tolist()[pixel_index][2:])}")
+
+graph(data_y=input_csv.values.tolist()[pixel_index][2:],title=str(pixel_index))
+# interpolate(raw_data=input_csv.values.tolist()[pixel_index][2:], x_values=input_csv.columns.tolist(),
+#             title=str(pixel_index), interpolation_points=['2019-06-04', '2019-07-19', '2019-07-19', '2019-08-23',
+#                                                           '2019-08-23', '2019-09-22', '2019-09-22', '2019-10-02',
+#                                                           '2019-10-17', '2019-11-06', '2019-12-11', '2019-12-31'])
