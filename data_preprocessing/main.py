b189b0af40c306d7c05bd3c946c6d6137203f43d
sudhanshu <sudhanshu.1.k@gmail.com>
2020-03-16 18:07:18 +0530

    Added comments

--- data_preprocessing/main.py
+++ data_preprocessing/main.py
@@ -3,16 +3,15 @@ import pandas as pd
 
 from data_preprocessing.interpolation import interpolate, graph
 
-# title = input("Band to plot : ")
-# pixel_index = int(input("Pixel Index : "))
-title = "NDVI"
-pixel_index = 0
+# Select Band and Data Point:
+title = input("Band to plot : ")
+pixel_index = int(input("Pixel Index : "))
 input_csv = pd.read_csv(os.path.abspath(__file__ + "/../../")+f"/data_2019/{title}.csv")
 
-# print(input_csv.columns.tolist())
-# for pixel_index in range(0, 27):
 
-# columnns = input_csv.columns.tolist()[2:]
+# print(input_csv.columns.tolist())                   Get list of columns
+
+# columnns = input_csv.columns.tolist()[2:]           Get entry for every new month for X-Axis labels
 # for entry in columnns:
 #     month = entry[5:7]
 #     print(month)
@@ -22,12 +21,13 @@ input_csv = pd.read_csv(os.path.abspath(__file__ + "/../../")+f"/data_2019/{titl
 #         labels.append(entry)
 #         indexes.append(columnns.index(entry))
 #         init_month = month
-#
-# print(labels)
-# print(indexes)
 
+
+# Print Min & Max values for the selected data point:
 print(f"Min value : {min(input_csv.values.tolist()[pixel_index][2:])} , Max value : {max(input_csv.values.tolist()[pixel_index][2:])}")
 
+
+# Play:
 graph(data_y=input_csv.values.tolist()[pixel_index][2:],title=str(pixel_index))
 # interpolate(raw_data=input_csv.values.tolist()[pixel_index][2:], x_values=input_csv.columns.tolist(),
 #             title=str(pixel_index), interpolation_points=['2019-06-04', '2019-07-19', '2019-07-19', '2019-08-23',
