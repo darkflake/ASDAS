c4b46c8a9867c5fae6c6c78e7ffbf01d65674663
sudhanshu <sudhanshu.1.k@gmail.com>
2020-03-19 18:55:15 +0530

    Function to create generalized graph

---
+++ processing/signal_matching.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from data_preprocessing import main
+
+
+def get_average_curve(input_csv: pd.DataFrame):
+    r"""
+    Find the generalized curve to represent the class
+
+    :param input_csv: raw class data
+    :return: data points for generalized curve
+
+    """
+    average_series = input_csv.mean(axis=0)
+    generalised = pd.DataFrame(average_series).transpose()
+    return generalised
+
+
+class_name, band_name, band_index, band_csv = main.get_data()
+interpolated_data, filtered_data = main.perform(band_csv)
+
+generalized = get_average_curve(band_csv)
+
+
+main.display(input_csv=generalized, name_of_band=band_name, pixel_index=band_index, interpolate_points=None, apply_filter=True, do_interpolate=False)
