68db05fda54874a4bd3da78912b478f561d95f46
sudhanshu <sudhanshu.1.k@gmail.com>
2020-03-19 19:57:34 +0530

    removed redundant code

--- data_preprocessing/main.py
+++ data_preprocessing/main.py
@@ -129,6 +129,4 @@ def write_csv(input_data: pd.DataFrame, name_of_class: str, file_name: str):
 # display(input_data=csv, pixel_index=index, name_of_band=band_name, do_interpolate=False, apply_filter=False,
 #         interpolate_points=interpolation_points)
 
-# _____________________________________________________________________________________________________________________
-# print(no_nan_csv.columns.tolist())                   Get list of columns
 
