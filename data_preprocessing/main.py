463182e6dc647c005e9fbeffb48a4d6ddc4d9f10
sudhanshu <sudhanshu.1.k@gmail.com>
2020-03-19 18:54:11 +0530

    modularized functions

--- data_preprocessing/main.py
+++ data_preprocessing/main.py
@@ -39,13 +39,15 @@ def fix_nan(input_data: pd.DataFrame):
 
 
 def get_data():
-    name_of_class = input("Select Class [Forests/Water] : ")
-    name_of_band = input("Band to plot [Blue/Green/Red/NIR/SWIR/SCL / NDVI] : ")
-    pixel_index = int(input("Pixel Index : "))
-    # name_of_band = 'NDVI'
-    # pixel_index = 0
+    # name_of_class = input("Select Class [Forests/Water] : ")
+    # name_of_band = input("Band to plot [Blue/Green/Red/NIR/SWIR/SCL / NDVI] : ")
+    # pixel_index = int(input("Pixel Index : "))
+    name_of_class = 'Forests'
+    name_of_band = 'NDVI'
+    pixel_index = 0
 
     input_csv = pd.read_csv(os.path.abspath(__file__ + "/../../") + f"/data_2019/csv/{name_of_class}/{name_of_band}.csv")  # Get csv
+
     return name_of_class, name_of_band, pixel_index, input_csv
 # _____________________________________
 
@@ -56,14 +58,14 @@ def display(input_csv: pd.DataFrame, name_of_band: str, pixel_index: int = 0, in
     Renders the graph for the given data for the given pixel index.
 
     :param input_csv: input data
-    :param pixel_index: which data point (pixel) to render : 0-27
+    :param pixel_index: which data point (pixel) to render : 0-699
     :param name_of_band: Red / Green / Blue / NIR / NDVI
     :param interpolate_points: list of points to perform interpolation between
     :param apply_filter: Apply and render Savitsky-Golay filter
     :param do_interpolate: Perform interpolation on data
     :return: None
     """
-    print(f"Min value : {min(input_csv.values.tolist()[index][2:])} , Max value : {max(input_csv.values.tolist()[index][2:])}")
+    print(f"Min value : {min(input_csv.values.tolist()[pixel_index][2:])} , Max value : {max(input_csv.values.tolist()[pixel_index][2:])}")
 
     if do_interpolate:
         interpolate(raw_data=input_csv.values.tolist()[pixel_index][2:], display=True, apply_filter=apply_filter,
@@ -74,36 +76,39 @@ def display(input_csv: pd.DataFrame, name_of_band: str, pixel_index: int = 0, in
 # _____________________________________
 
 
-def perform(input_csv: pd.DataFrame):
+def perform(input_csv: pd.DataFrame, band_index: int, interpolation_points: list):
     r"""
     Perform pre-processing on data. ( Handle missing values + Cloud Correction using Interpolation + SavGol filtering )
+
     :param input_csv: raw data
+    :param band_index: which data point (pixel) to render : 0-699
+    :param interpolation_points: list of points to perform interpolation between
     :return: dictionary with interpolated and filtered DataFrames
     """
-    no_nan_csv = fix_nan(csv)
+    no_nan_csv = fix_nan(input_csv)
 
-    interpolated_csv = apply_interpolation(data_csv=no_nan_csv, index=index, interpolation_points=interpolation_points)
+    interpolated_csv = apply_interpolation(data_csv=no_nan_csv, index=band_index, interpolation_points=interpolation_points)
 
-    filtered_csv = apply_savgol(data_csv=interpolated_csv, index=index, window=7, order=3)
+    filtered_csv = apply_savgol(data_csv=interpolated_csv, index=band_index, window=7, order=3)
 
     processed = {'Interpolated': interpolated_csv, 'Filtered': filtered_csv}
     return processed
 
 
 # Play:
-interpolation_points = ['2019-06-04', '2019-07-19', '2019-07-19', '2019-08-23', '2019-08-23', '2019-09-22',
-                        '2019-09-22', '2019-10-02', '2019-10-17', '2019-11-06', '2019-12-11', '2019-12-31']
-
-class_name, band_name, index, csv = get_data()
-print("___")
-
-interpolated_data, filtered_data = perform(csv)
-
-
-# write_csv(filtered_csv, class_name, f"filtered-{band_name}")
-
-display(input_csv=csv, pixel_index=index, name_of_band=band_name, do_interpolate=False, apply_filter=True,
-        interpolate_points=interpolation_points)
+# interpolation_points = ['2019-06-04', '2019-07-19', '2019-07-19', '2019-08-23', '2019-08-23', '2019-09-22',
+#                         '2019-09-22', '2019-10-02', '2019-10-17', '2019-11-06', '2019-12-11', '2019-12-31']
+#
+# class_name, band_name, index, csv = get_data()
+# print("___")
+#
+# interpolated_data, filtered_data = perform(csv)
+#
+#
+# # write_csv(filtered_csv, class_name, f"filtered-{band_name}")
+#
+# display(input_csv=csv, pixel_index=index, name_of_band=band_name, do_interpolate=False, apply_filter=False,
+#         interpolate_points=interpolation_points)
 
 # _____________________________________________________________________________________________________________________
 # print(no_nan_csv.columns.tolist())                   Get list of columns
