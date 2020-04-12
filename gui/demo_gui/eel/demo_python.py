14110023c268b456f97784abbbe99d23a2fa73f2
siddhi <siddhiasonar@gmail.com>
2020-04-12 23:01:51 +0530

    demo gui

--- gui/demo_gui/eel/demo_python.py
+++ gui/demo_gui/eel/demo_python.py
@@ -4,10 +4,44 @@ import eel
 import cv2 as cv
 import datetime
 from PIL import Image
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
 eel.init('web')
 @eel.expose
+def piechart():
+    df=pd.read_csv('6580631000-1547012631000.csv')
+    country_data = df["index"]
+    medal_data = df["B11"]
+    colors = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#8c564b"]
+    plt.pie(medal_data, labels=country_data,autopct='%1.1f%%')
+    plt.title('My Tasks')
+    plt.show()
+
+
+
+'''
+# Data to plot
+labels = 'Python', 'C++', 'Ruby', 'Java'
+sizes = [215, 130, 245, 210]
+colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue']
+explode = (0.1, 0, 0, 0)  # explode 1st slice
+
+
+def plot_piechart():
+    plt.pie(sizes, explode=explode, labels=labels, colors=colors,
+    autopct='%1.1f%%', shadow=True, startangle=140)
+
+    plt.axis('equal')
+    plt.show()
+
+    per1 = pd.date_range(start='1-1-2018',
+                     end='1-05-2018', freq='5H')
+
+for val in per1:
+    print(val)
+
 
 def   show_image():
     img=cv.imread('walpaper.jpg')
@@ -19,37 +53,12 @@ def   show_image():
     #img = Image.open('C:/Users/HP/Pictures/eth.PNG')
     #img.show()
     # os.startfile('C:/Users/HP/Pictures/Saved Pictures/walpaper.jpg')
-
-
 '''
-eel.expose()
-def write():
-    str="this is demo project"
-    print(str)'''
 
 
 
 
 
-eel.init('web', allowed_extensions=['.js', '.html'])
-
-eel.init('web')
-@eel.expose                         # Expose this function to Javascript
-def say_hello_py(x):
-    print('Hello from %s' % x)
-
-
-say_hello_py('Python World!')
-#eel.say_hello_js('Python World!')   # Call a Javascript function
-
-@eel.expose
-def date():
-
-    x = datetime.datetime.now()
-
-    print(x.year)
-
-
-eel.start('demo_html.html', mode='chrome app')
+eel.start('demo_html.html')
 
 
