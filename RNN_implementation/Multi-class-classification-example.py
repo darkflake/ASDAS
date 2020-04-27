460404231d2449d83ef5dfcbede9bbb936ad985b
shubham3542 <42933263+shubham3542@users.noreply.github.com>
2020-04-27 14:23:44 +0530

    Multi-class-classification-example.py: Shuffled data importing

--- RNN_implementation/Multi-class-classification-example.py
+++ RNN_implementation/Multi-class-classification-example.py
@@ -1,27 +1,29 @@
 import datetime
-
-import keras
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation
-from keras.optimizers import SGD
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation
+from tensorflow.keras.optimizers import SGD, Adam
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+from sklearn.model_selection import train_test_split
 
 
 def data_preprocessing():
-    x_train = pd.read_csv(f'train.csv')
+    dataframe = pd.read_csv(f'FinalData_band.csv')
+    x_train, x_test = train_test_split(dataframe, test_size=0.33, shuffle=True)
+    #x_train = pd.read_csv(f'train.csv')
     train_label = x_train.pop('Label')
     train_label = train_label.to_numpy()
     y_train = keras.utils.to_categorical(train_label, num_classes=5)
-    x_train = x_train.div(10000).round(7)
+    x_train = x_train.div(1000).round(7)
     x_train = x_train.to_numpy()
 
-    x_test = pd.read_csv(f'test.csv')
+    #x_test = pd.read_csv(f'test.csv')
     test_label = x_test.pop('Label')
     test_label = test_label.to_numpy()
     y_test = keras.utils.to_categorical(test_label, num_classes=5)
-    x_test = x_test.div(10000).round(7)
+    x_test = x_test.div(1000).round(7)
     x_test = x_test.to_numpy()
 
     return x_train, y_train, x_test, y_test
@@ -31,30 +33,35 @@ def network():
     x_train, y_train, x_test, y_test = data_preprocessing()
     model = Sequential()
     model.add(Dense(4, activation='relu', input_dim=5))
-    model.add(Dropout(0.2))
+    # model.add(Dropout(0.2))
     model.add(Dense(3, activation='relu'))
-    model.add(Dropout(0.2))
+    # model.add(Dropout(0.2))
     model.add(Dense(4, activation='relu'))
-    model.add(Dropout(0.2))
+    # model.add(Dropout(0.2))
     model.add(Dense(5, activation='softmax'))
 
     sgd = SGD(lr=0.01, decay=1e-6, momentum=0.1, nesterov=True)
+    #sgd = SGD(lr=0.001)
 
     model.compile(loss='categorical_crossentropy',
                   optimizer=sgd,
                   metrics=['accuracy'])
 
+    log_dir = "logs\\fit\\" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir)
+
     model.fit(x_train, y_train,
-              epochs=3000,
+              epochs=500,
               batch_size=25,
+              callbacks=[tensorboard_callback],
               )
 
-    score = model.evaluate(x_train, y_train, batch_size=25)
+    score = model.evaluate(x_test, y_test, batch_size=25)
     print(model.metrics_names)
     print(score)
 
-    print(model.predict(x_train, batch_size=None, verbose=0, steps=None))
-    print(y_train)
+    # print(model.predict(x_test, batch_size=None, verbose=0, steps=None))
+    # print(y_train)
 
 
 network()
